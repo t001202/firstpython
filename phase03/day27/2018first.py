94e52d75e256964a166e405bf8df4d29a46e987d
王亚西 <575719022@qq.com>
2018-04-05 14:21:24 +0800

    111

--- phase03/day27/2018first.py
+++ phase03/day27/2018first.py
@@ -2,5 +2,16 @@
 # -*- coding: utf-8 -*-
 __author__ = "WangYx"
 
+print("我要开始学习Python,坚持下去")
 
-print("我要开始学习Python , 坚持下去")
+
+class SqlPool:
+	__instance = None
+
+	@staticmethod
+	def getInstance():
+		if SqlPool.__instance is not None:
+			return SqlPool.__instance
+		else:
+			SqlPool.__instance = SqlPool()
+			return SqlPool.__instance
